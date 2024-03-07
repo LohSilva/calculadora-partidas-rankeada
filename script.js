ede2a179bd6d539823aa7451353081063b241368
LohSilva <lo-ruama_sp@hotmail.com>
2024-03-06 23:23:17 -0300

    Criando Funções

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Iniciando Desafio - Calculadora de partidas Rankeadas")
+let resultado = saldoVitorias(30, 20);
+let nivel = " ";
+
+//Criando função para determinar o saldo de vitórias
+function saldoVitorias(vitorias, derrotas){
+    let saldo = vitorias - derrotas;
+    return saldo;
+}
+
+//Estrutura de repetição para encontrar o nível do herói
+switch (true){
+    case resultado <= 10:
+    nivel = "Ferro";
+    break;
+}
+
+console.log("O Herói tem de saldo de " + resultado + " está no nível de " + nivel);
