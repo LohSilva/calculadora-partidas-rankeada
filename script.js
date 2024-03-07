07dcffebd2fcc2e4074ffc30bff498300249902b
LohSilva <lo-ruama_sp@hotmail.com>
2024-03-07 18:20:47 -0300

    Criando Funções

--- script.js
+++ script.js
@@ -1,35 +1,39 @@
-let resultado = saldoVitorias(150, 20);
-let nivel = " ";
+//Calculadora de partidas Rankeadas
+let resultado = saldoVitorias(100, 20);
+let nivel = nivelHeroi();
 
 //Criando função para determinar o saldo de vitórias
-function saldoVitorias(vitorias, derrotas){
+function saldoVitorias(vitorias, derrotas) {
     let saldo = vitorias - derrotas;
     return saldo;
 }
 
-//Estrutura de repetição para encontrar o nível do herói
-switch (true){
-    case resultado <= 10:
-    nivel = "Ferro";
-    break;
-    case resultado >= 11 && resultado <= 20:
-    nivel = "Bronze";
-    break;
-    case resultado >= 21 && resultado <= 50:
-    nivel = "Prata";
-    break;
-    case resultado >= 51 && resultado <= 80:
-    nivel = "Ouro";
-    break;
-    case resultado >= 81 && resultado <= 90:
-    nivel = "Diamante";
-    break;
-    case resultado >= 91 && resultado <= 100:
-    nivel = "Lendário";
-    break;
-    default:
-    nivel = "Imortal";
-    break;
+//Função para verificar nivel do herói
+function nivelHeroi(nivel) {
+    switch (true) {
+        case resultado <= 10:
+            nivel = "Ferro";
+            break;
+        case resultado >= 11 && resultado <= 20:
+            nivel = "Bronze";
+            break;
+        case resultado >= 21 && resultado <= 50:
+            nivel = "Prata";
+            break;
+        case resultado >= 51 && resultado <= 80:
+            nivel = "Ouro";
+            break;
+        case resultado >= 81 && resultado <= 90:
+            nivel = "Diamante";
+            break;
+        case resultado >= 91 && resultado <= 100:
+            nivel = "Lendário";
+            break;
+        default:
+            nivel = "Imortal";
+            break;
+    }
+    return nivel;
 }
 
 //Saída
